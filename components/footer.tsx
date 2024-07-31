b68ec79dcf2a91cbcf9bea53f54d35e00ab4c5c6
Ofer Sadan <ofersadan85@gmail.com>
2024-07-31 09:35:58 +0300

    better footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import CopyButton from "./copy-button";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 const Footer = () => {
   const email = "ofersadan85@gmail.com";
 
   return (
-    <footer className="flex items-center justify-between bottom-0 w-full p-4 text-muted-foreground text-sm">
-      <p>© 2024 All rights reserved by Ofer Sadan</p>
+    <footer className="flex max-sm:flex-col items-center justify-center gap-4 bottom-0 w-full p-4 text-muted-foreground text-sm">
       <div className="items-center text-center">
         <Button variant="link" className="p-2">
           <a href="mailto:ofersadan85@gmail.com" target="_blank" className="text-base">
@@ -19,6 +19,22 @@ const Footer = () => {
           toastText="Copied email address to clipboard"
         />
       </div>
+      <Button variant="link" className="p-2">
+        <a href="https://github.com/ofersadan85" target="_blank" rel="noreferrer" className="text-base flex gap-2">
+          GitHub <GitHubLogoIcon className="w-6 h-6" />
+        </a>
+      </Button>
+      <Button variant="link" className="p-2">
+        <a
+          href="https://www.linkedin.com/in/ofer-sadan"
+          target="_blank"
+          rel="noreferrer"
+          className="text-base flex gap-2"
+        >
+          LinkedIn <LinkedInLogoIcon className="w-6 h-6" />
+        </a>
+      </Button>
+      <p>© 2024 All rights reserved by Ofer Sadan</p>
     </footer>
   );
 };
